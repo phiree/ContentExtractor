def7fb81e562cc5c94aacbbf9f62b592da33f78e
phiree <phiree@gmail.com>
2012-08-15 01:15:40 +0800

    add ruleset

---
+++ CE.Model/Rule/RuleSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CE.Model.Rule
+{
+   /// <summary>
+   /// 用于提取特定内容的一组rule
+   /// </summary>
+    public class RuleSet
+    {
+
+    }
+}
