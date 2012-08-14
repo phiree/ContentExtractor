bd99a3cec4b69b797ba7f6e1b62d40acdba0a4ca
phiree <phiree@gmail.com>
2012-08-14 21:28:36 +0800

    rule define

---
+++ CE.Model/Rule/BaseRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CE.Model.Rule
+{
+    public class BaseRule
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public abstract string FilterUsingRule(string rawContent);
+    }
+}
---
+++ CE.Model/Rule/BeginEndRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CE.Model.Rule
+{
+    /// <summary>
+    /// 能够获取某项特定内容的规则集合
+    /// </summary>
+    public class BeginEndRule:IRule
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int OrderNumber { get; set; }
+        public string RawContent { get; set; }
+       
+        public TargetSite Site { get; set; }
+
+        public string ExcuteFilter(string rawContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
