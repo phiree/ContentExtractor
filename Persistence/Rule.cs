ab52d8ea7c0b077e04f1c5ce25d3e5c761df8c83
phiree <phiree@gmail.com>
2012-08-21 09:46:54 +0800

    add persistence priject

---
+++ Persistence/Rule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IPersistence;
+namespace Persistence
+{
+    public class Rule:IRule
+    {
+        public void SaveRule(CE.Domain.Rule.BaseRule rule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
