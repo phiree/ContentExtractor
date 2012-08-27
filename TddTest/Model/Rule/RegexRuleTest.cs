8a3b299cfd432ff4cb7b640fbd3eb6518946bfab
phiree <phiree@gmail.com>
2012-08-27 12:10:12 +0800

    RegexRule

--- TddTest/Model/Rule/RegexRuleTest.cs
+++ TddTest/Model/Rule/RegexRuleTest.cs
@@ -14,9 +14,36 @@ namespace TddTest.Model.Rule
         public void FilterUsingRuleTest()
         {
 
-            string rawContent = CE.Component.HtmlHandler.ReadHtml(@"D:\downloading\2113864687.html");
-            Assert.AreEqual("启园$#$60$#$48$#$",
-                new RegexRule(@"<div class=""st_bod"">(?<name>.*?)</div>.*?<span class=""parGd"">.(?<price>\d+)</span>").FilterUsingRule(ref rawContent));
+            string rawContent =@"<p class=""tkType"">
+													ddsn class=""tkSpan"" style=""cursor: default"" id=""se_title_1"">
+														<span>鼎湖峰</span>
+													</span>
+												</p>
+													<span fan=""2"" class=""xjq_new"" _left=""420"" _top=""581"" _width=""115"" _height=""22"">
+														<s fxprice=""2"" danbaotype=""0"" class=""return"">
+															<span class=""return_tt clearfix""></span><span class=""return_ct""><span class=""nob"">¥</span>2</span>
+														</s>
+													</span>
+											</td>
+											<td class=""sp_price"">
+												<span class=""parGd"">¥60</span>
+											</td>
+											<td>
+												<dl class=""saveMne"">
+													<dt><span class=""Mne"">¥</span>48</dt>
+												</dl>
+											</td>
+											<td>
+												<span id=""onlinepay_0_0"">景区支付</span>
+											</td>
+											<td>
+												<a title=""预订"" href=""javascript:void(0)"" onclick=""GetOrderUrl(2851,1381);return false;"" class=""yd_butm"" rel=""nofollow"">
+													预&nbsp;订</a>
+											</td>
+										</tr>
+										";
+            Assert.AreEqual("鼎湖峰$#$60$#$48$#$",
+                new RegexRule(@"id=""se_title_\d+"">.*?<span>(?<t_name>.*?)</span>.*""parGd"">.(?<t_price1>\d+)</span>.*?""Mne"">.</span>(?<price2>\d+)</dt>").FilterUsingRule(ref rawContent));
            // new RegexRule(@"id=""se_title_\d+""\>\r*\s+\<span\>(?<t_name>.*?)\</span\>").FilterUsingRule(ref rawContent));
             /*
              name: id=""se_title_1"">\s*<span>(?<t_name>.*?)</span>
