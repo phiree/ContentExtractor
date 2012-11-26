f8d14b29dd1c44a57776ba2e0eb2b71b34175c76
phiree <phiree@gmail.com>
2012-11-26 11:03:57 +0800

    confict merge

---
+++ CE.Component/UrlSpider/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CE.Component.UrlSpider
+{
+    /// <summary>
+    /// 给定 起始URL,获得所有需要的链接
+    /// 
+    /// </summary>
+    class Class1
+    {
+    }
+}
